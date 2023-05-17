560716742b0087d428cf0979bb0192e3a7777671
Ekansh <zethyst@protonmail.com>
2023-05-18 02:44:44 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+<h1 align="center">Hackerman: Unleash Your Inner Hacker</h1>
+<p style="font-style:italic;">Welcome to Hackerman, your virtual playground for all things hacking and password generation! Immerse yourself in the thrilling world of cybersecurity as you embark on a journey to generate unique passwords and experience the exhilaration of being a digital virtuoso.
+
+In the realm of Hackerman, the lines between reality and fiction blur. The sleek black and vibrant green interface instantly transports you into the iconic atmosphere of hacking adventures. Get ready to channel your inner hacker as you dive into the matrix of password generation.
+
+With Hackerman, the process is simple yet captivating. Enter a username of your choice, and watch as our sophisticated algorithms conjure up a password tailored to the essence of that user. Our database serves as a digital vault, securely storing the passwords created for each user. It's like having the power to crack codes and unlock secret vaults, all at your fingertips.
+
+Hackerman is more than just a password generator. It's an homage to the iconic aesthetics of hacking culture, where the combination of black and green creates an electrifying ambiance that fuels your imagination. With every keystroke, you're transported deeper into a world where technology and creativity converge.
+
+Get ready to enter the matrix. Hackerman awaits your command.</p>
+<div align = "center">
+<img src="https://forthebadge.com/images/badges/validated-html5.svg">
+<img src="https://forthebadge.com/images/badges/uses-css.svg">
+<img src="https://forthebadge.com/images/badges/made-with-javascript.svg">
+</div>
---
+++ script.js
@@ -0,0 +1,72 @@
+
+var input = document.getElementById("user");
+input.addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById("pwd").click();
+    }
+});
+
+pwd.addEventListener("click", () => {
+    let t = document.getElementsByClassName("userbox")[0];
+    t.style.display = "none";
+    let c = document.getElementsByClassName("container")[0];
+    c.style.width = "80vw";
+    c.style.height = "55vh";
+    var x = document.getElementById("user").value;
+
+    let p1 = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Initializing Hack Program...");
+        }, 1200);
+    });
+    let p2 = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Searching [ " + x + " ] username ...");
+        }, 2700);
+    });
+    let p3 = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Username found => [ " + x + " ]");
+        }, 6000);
+    });
+    let p4 = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Password saved in database...");
+        }, 10000);
+    });
+    let p5 = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Connecting to Instagram...");
+        }, 12000);
+    });
+    let p6 = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Connected.");
+        }, 16000);
+    });
+    p1.then((value) => {
+        document.getElementById("demo").innerHTML = value;
+        var prevHTML = document.getElementById("demo").innerHTML;
+    });
+    p2.then((value) => {
+        let temp = document.getElementById("demo").innerHTML;
+        document.getElementById("demo").innerHTML = temp + "<br><br>" + value;
+    });
+    p3.then((value) => {
+        let temp = document.getElementById("demo").innerHTML;
+        document.getElementById("demo").innerHTML = temp + "<br><br>" + value;
+    });
+    p4.then((value) => {
+        let temp = document.getElementById("demo").innerHTML;
+        document.getElementById("demo").innerHTML = temp + "<br><br>" + value;
+    });
+    p5.then((value) => {
+        let temp = document.getElementById("demo").innerHTML;
+        document.getElementById("demo").innerHTML = temp + "<br><br>" + value;
+    });
+    p6.then((value) => {
+        let temp = document.getElementById("demo").innerHTML;
+        document.getElementById("demo").innerHTML = temp + "<br><br>" + value;
+    });
+});
